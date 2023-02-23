5be53861c90d6dc6be382daad1b481334901abc4
szumi112 <mateusz.szumilo@dornach.eu>
2023-02-23 23:03:19 +0100

    chore: update linnk for backend

--- moving-react/frontend/src/App.jsx
+++ moving-react/frontend/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   const [showReviews, setShowReviews] = useState(3);
 
   useEffect(() => {
-    fetch("http://zulak-moving.onrender.com/reviews")
+    fetch("http://moving-zulak.onrender.com/reviews")
       .then((response) => response.json())
       .then((data) => setReviews(data))
       .catch((error) => console.error("Error fetching reviews:", error));
